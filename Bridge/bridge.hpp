86bb464825919ec75aa31f9cc7bee6023d72c9b2
ksoczowka <ksoczowka.ks@gmail.com>
2024-05-09 22:21:23 +0200

    added imlementation for bridge design pattern

--- Bridge/bridge.hpp
+++ Bridge/bridge.hpp
@@ -1,9 +1,64 @@
 #pragma once
 
-class Implementation {
+#include <memory>
+#include <string>
 
+class Implementation {
+ public:
+    Implementation() {}
+
+    std::string getVersion() const { 
+        return "Implementation()"; 
+    }
+};
+
+class NewImplementationA : public Implementation {
+ public:
+    std::string getVersion() const {
+        return "NewImplementationA()";
+    }
+};
+
+class NewImplementationB : public Implementation {
+ public:
+    std::string getVersion() const {
+        return "NewImplementationB()";
+    }
 };
 
 class Abstraction {
+ public:
+    Abstraction() {
+        implementation_ = nullptr;
+    }
+    Abstraction(std::shared_ptr<Implementation> Implementation) : implementation_(Implementation) {}
+    std::string info() const {
+        return "Abstraction()";
+    }
+    void setImplementation(std::shared_ptr<Implementation> other) {
+        implementation_ = other;
+    }
 
+ protected:
+    std::shared_ptr<Implementation> implementation_;
+};
+
+class NewAbstractionA : public Abstraction {
+ public:
+    std::string info() const {
+        return "NewAbstractionA()";
+    }
+    std::string getImplementationVersionn() {
+        return implementation_->getVersion();
+    }
+};
+
+class NewAbstractionB : public Abstraction {
+ public:
+    std::string info() const {
+        return "NewAbstractionB()";
+    }
+    std::string getImplementationVersionn() {
+        return implementation_->getVersion();
+    }
 };
